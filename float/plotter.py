71cc9065c8cd041a7257c87ea6752a75a541832f
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-10 13:45:10 -0800

    Update plotter

--- float/plotter.py
+++ float/plotter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pickle
 import os
+import pandas as pd
 
 LOG_INDEX = -1
 
@@ -10,7 +11,14 @@ def plot_log():
 
     with open(os.path.join(name), "rb") as f:
         data = pickle.load(f)
-        print(data)
+        df = pd.DataFrame.from_dict(data)
+
+    plt.plot(df['float_time'], df['mb']/1013)
+    plt.title("Float Pressure Data")
+    plt.xlabel("Time since recording was started (seconds)")
+    plt.ylabel("Pressure (atm)")
+    plt.savefig(f"float\\figures\\{fnames[LOG_INDEX]}_pressure.png")
+    plt.close()
 
 if __name__ == "__main__":
     plot_log()
