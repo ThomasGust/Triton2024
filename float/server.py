b7438a566e0a2d45b88be7f138b16108cc9c65c0
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-02-15 17:42:09 -0800

    Update server.py

--- float/server.py
+++ float/server.py
@@ -27,94 +27,99 @@ class BluetoothServer:
     def __init__(self, command_port, command_uuid, sensor_port, sensor_uuid, ping_port, ping_uuid):
         self._command_port = command_port
         self.command_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        self.command_sock.bind(("", self.port))
+        self.command_sock.bind(("", self._command_port))
         self.command_sock.listen(1)
-        self.command_port = self.server_sock.getsockname()[1]
+        self.command_port = self.command_sock.getsockname()[1]
 
         self.command_uuid = command_uuid
         # Instead of running everything over 1 socket, we will be using an architecture where we use multiple sockets to send different data.
-        bluetooth.advertise_service(self.command_sock, "FloatCommander", service_id=self.command_uuid,
-                                    service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+        bluetooth.advertise_service(self.command_sock, "FloatServer", service_id=self.command_uuid,
+                                    service_classes=[bluetooth.SERIAL_PORT_CLASS],
                                     profiles=[bluetooth.SERIAL_PORT_PROFILE])
         
         self._sensor_port = sensor_port
         self.sensor_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        self.sensor_sock.bind(("", self.port))
+        self.sensor_sock.bind(("", self._sensor_port))
         self.sensor_sock.listen(1)
-        self.sensor_port = self.server_sock.getsockname()[1]
+        self.sensor_port = self.sensor_sock.getsockname()[1]
 
         self.sensor_uuid = sensor_uuid
 
         bluetooth.advertise_service(self.sensor_sock,"SensorService", service_id=self.sensor_uuid,
-                                    service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                                    service_classes=[bluetooth.SERIAL_PORT_CLASS],
                                     profiles=[bluetooth.SERIAL_PORT_PROFILE])
         
         # The purpose of the ping service is to let the rest of the threads understand when the float is in range
         self._ping_port = ping_port
         self.ping_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        self.ping_sock.bind(("", self.port))
+        self.ping_sock.bind(("", self._ping_port))
         self.ping_sock.listen(1)
-        self.ping_port = self.server_sock.getsockname()[1]
+        self.ping_port = self.ping_sock.getsockname()[1]
         
         self.ping_uuid = ping_uuid
 
         bluetooth.advertise_service(self.ping_sock,"PingService", service_id=self.ping_uuid,
-                                    service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                                    service_classes=[bluetooth.SERIAL_PORT_CLASS],
                                     profiles=[bluetooth.SERIAL_PORT_PROFILE])
         
         self.on = True
+        self.connectable = True
     
     def command_handler(self):
         while self.on:
-            print("Waiting for connection on RFCOMM channel: ", self.command_port)
+            print("Waiting for COMMAND connection on RFCOMM channel: ", self.command_port)
             self.handle_command()
 
     def sensor_handler(self):
         while self.on:
-            print("Waiting for connection on RFCOMM channel: ", self.sensor_port)
+            print("Waiting for SENSOR connection on RFCOMM channel: ", self.sensor_port)
             self.handle_sensor()
         
     def ping_handler(self):
         while self.on:
-            print("Waiting for connection on RFCOMM channel: ", self.ping_port)
+            print("Waiting for PING connection on RFCOMM channel: ", self.ping_port)
             self.handle_ping()
 
     def handle_client(self):
-        while self.on:
-            command_thread = threading.Thread(target=self.command_handler)
-            sensor_thread = threading.Thread(target=self.sensor_handler)
-            ping_thread = threading.Thread(target=self.ping_handler)
-            command_thread.start()
-            sensor_thread.start()
-            ping_thread.start()
-            command_thread.join()
-            sensor_thread.join()
-            ping_thread.join()
+        command_thread = threading.Thread(target=self.command_handler)
+        sensor_thread = threading.Thread(target=self.sensor_handler)
+        ping_thread = threading.Thread(target=self.ping_handler)
+    
+        command_thread.start()
+        sensor_thread.start()
+        ping_thread.start()
+
+        command_thread.join()
+        sensor_thread.join()
+        ping_thread.join()
     
     def handle_sensor(self):
         # This thread should constantly deserialize any data spat out by the sensor
         # Our client should spit out log data every 5 seconds while it is connected
+        sensor_sock, sensor_info = self.sensor_sock.accept()
+        print("Accepted SENSOR connection from", sensor_info)
         while self.on:
-            lines = []
-            done = False
-            while not done:
-                data = self.sensor_sock.recv(4096)
-                if data == b'':
-                    time.sleep(2)
-                else:
-                    deserialized = pickle.loads(data)
-                    if deserialized == "EOF":
-                        done = True
+            if self.connectable:
+                lines = []
+                done = False
+                while not done:
+                    data = sensor_sock.recv(4096)
+                    if data == b'':
+                        time.sleep(2)
                     else:
-                        lines.append(deserialized)
-            
-            # Now we will save the lines to our log file
-            with open(fname, "wb") as f:
-                pickle.dump(lines, f)
+                        deserialized = pickle.loads(data)
+                        if deserialized == "EOF":
+                            done = True
+                        else:
+                            lines.append(deserialized)
+                
+                # Now we will save the lines to our log file
+                with open(fname, "wb") as f:
+                    pickle.dump(lines, f)
 
-    def handle_client(self):
-        client_sock, client_info = self.server_sock.accept()
-        print("Accepted connection from", client_info)
+    def handle_command(self):
+        client_sock, client_info = self.command_sock.accept()
+        print("Accepted COMMAND connection from", client_info)
         print("o: stutter out")
         print("i: stutter in")
         print("s: stop")
@@ -126,21 +131,38 @@ class BluetoothServer:
 
         #I will add the key for profiling here later.
         while True:
-            cmd = input("Enter Command: ")
-            if cmd == "quit":
-                break
-            client_sock.send(cmd)
-            if cmd == "x":
-                client_sock.close()
-
-        print("Disconnected.")
-
-        client_sock.close()
+            if self.connectable:
+                cmd = input("Enter Command: ")
+                if self.connectable:
+                    client_sock.send(cmd)
+    
+    def handle_ping(self):
+        ping_sock, ping_info = self.ping_sock.accept()
+        ping_sock.settimeout(5)
+        print("Accepted PING connection from", ping_info)
+        while self.on:
+            try:
+                data = ping_sock.recv(1024).decode("utf-8")
+                print(data)
+                if self.connectable == False:
+                    print("Server is responding")
+                self.connectable = True
+                ping_sock.send("pong")
+            except:
+                if self.connectable == True:
+                    print("Server is not responding")
+                self.connectable = False
+            time.sleep(1)
+        ping_sock.close()
 
 if __name__ == "__main__":
-    print(generate_uuid)
-    id = "71c94c53-28c7-43a3-b05f-0ffd96fcd2e3"
-    port = bluetooth.PORT_ANY
+    command_id = "71c94c53-28c7-43a3-b05f-0ffd96fcd2e3"
+    sensor_id = "34267a21-7847-4cda-95fe-8e96dcd6b2b6"
+    ping_id = "ca9237cc-7d72-46da-9e8f-33d3e29643aa"
 
-    server = BluetoothServer(port, id)
-    server.handle_clients()
+    command_port = bluetooth.PORT_ANY
+    sensor_port = bluetooth.PORT_ANY
+    ping_port = bluetooth.PORT_ANY
+
+    server = BluetoothServer(command_port, command_id, sensor_port, sensor_id, ping_port, ping_id)
+    server.handle_client()
