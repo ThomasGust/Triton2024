9509348e7187492d4c49b9b5443513994ce08efc
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2024-03-12 17:10:34 -0700

    Create camera

---
+++ control/motion_tracking.py
@@ -0,0 +1,9 @@
+import cv2
+
+width=640
+height=360
+cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
+cam.set(cv2.CAP_PROP_FPS, 30)
+cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
